7f15f72de53aadba84b047ef1f85f27bdc5ad81a
Subhajit Sahu <wolfram77@gmail.com>
2018-10-10 10:13:56 +0530

    sunoo na code

--- index.js
+++ index.js
@@ -1,74 +1,29 @@
+const isVideo = require('is-video');
 const sqlite = require('sqlite');
 const wiki = require('wikijs').default;
 const youtube = require('@wikipedia-tts/youtube');
+const english = require('@wikipedia-tts/english');
+const video = require('@wikipedia-tts/video');
 const path = require('path');
-const cp = require('child_process');
 
 
 // Global variables
 const E = process.env;
-process.chdir('C:\\Users\\wolfram\\.google');
-const DB = E['WIKIPEDIA_TTS_DB']||'crawl.db';
-const OUTPUT = E['WIKIPEDIA_TTS_OUTPUT']||'./';
-const YOUTUBE_VIDEO = {
-  title: E['YOUTUBE_VIDEO_TITLE']||'${title}',
-  description: E['YOUTUBE_VIDEO_DESCRIPTION']||'${description}',
-  tags: E['YOUTUBE_VIDEO_TAGS']||'${tags}',
-  categoryId: 22
-};
-const YOUTUBE_RETRY = parseInt(E['YOUTUBE_RETRIES'], 10)||3;
-const CP = {
-  sync: true,
-  stdio: [0, 1, 2]
-};
+const DB = E['WTTS_DB']||'crawl.db';
 
 
-// Execute child process, return promise.
-function cpExec(cmd, o) {
-  o = Object.assign({}, CP, o);
-  if(o.sync) return Promise.resolve({stdout: cp.execSync(cmd, o)});
-  return new Promise((fres, frej) => {
-    cp.exec(cmd, o, (err, stdout, stderr) => {
-      if(err) frej(err);
-      else fres({stdout, stderr});
-    });
-  });
-};
-
-// Get TTS audio/video of wikipedia page.
-async function wikipediaTts(out, nam, o) {
-  var o = o||{};
+// Upload Wikipedia page TTS to Youtube.
+function wikipediaTts(out, nam, o) {
   var pag = await wiki().page(nam);
   var txt = await pag.content();
   var img = await pag.mainImage();
-  await english(out, txt, img, o.english);
-  return pag;
-};
-
-// Upload file to Youtube.
-function youtubeUpload(val, o) {
-  var o = Object.assign({retry: YOUTUBE_RETRY}, o);
-  o.video = Object.assign({}, YOUTUBE_VIDEO, o.video);
-  var v = o.video;
-  v.filepath = val.filepath;
-  v.title = v.title.replace(/\${title}/g, val.title);
-  v.description = v.description.replace(/\${title}/g, val.title);
-  v.description = v.description.replace(/\${description}/g, val.description);
-  v.description = _.escapeRegExp(v.description);
-  v.tags = v.tags.replace(/\${tags}/g, val.tags);
-  console.log(`youtubeuploader -description "${v.description}" -filename "${v.filepath}" -tags "${v.tags}" -title "${v.title}"`);
-  return cpExec(`youtubeuploader -description "${v.description}" -filename "${v.filepath}" -tags "${v.tags}" -title "${v.title}"`, CP);
-};
-
-// Upload page to Youtube.
-async function uploadPage(nam, o) {
-  var o = Object.assign({output: OUTPUT}, o);
-  var out = path.join(o.output, nam+'.mp4');
-  var pag = await wikipediaTts(out, nam, o);
-  var sum = await pag.summary();
-  var tags = nam.toLowerCase().split(/\W+/);
-  await youtubeUpload({filepath: out, title: nam, description: sum, tags}, o.youtube);
-  return pag;
+  var description = await pag.summary();
+  var tags = nam.toLowerCase().split(/\W+/).join(',');
+  var val = {title: nam, description, tags};
+  var ext = path.extname(out||'a.json').toLowerCase();
+  if(ext==='.json') return youtube(out, txt, img, val, o).then(() =>  pag);
+  if(isVideo(out)) return video(out, txt, img, o).then(() => pag);
+  return english(out, txt, o).then(() => pag);
 };
 
 // Setup crawl list.
@@ -100,8 +55,8 @@ async function update(db, nam, val) {
 };
 
 // Upload a page in crawl list.
-async function upload(db, nam) {
-  var pag = await uploadPage(nam), p = [];
+async function upload(db, nam, o) {
+  var pag = await wikipediaTts(null, nam, o), p = [];
   await db.exec('UPDATE "pages" SET "uploaded" = 1 WHERE "title" = ?', [nam]);
   for(var lnk of await pag.links())
     p.push(db.exec('UPDATE "pages" SET "references" = "references" + 1 WHERE "title" = ?', [lnk]));
@@ -110,11 +65,11 @@ async function upload(db, nam) {
 };
 
 // Crawl a page.
-async function crawl(db) {
+async function crawl(db, o) {
   var whr = '"uploaded" = 0';
   var ord = '"priority" DESC, "references" DESC';
   var row = await db.get(`SELECT * FROM "pages" WHERE ${whr} ORDER BY ${ord} LIMIT 1`);
-  return row? await upload(row.title):null;
+  return row? await upload(db, row.title, o):null;
 };
 module.exports = wikipediaTts;
 wikipediaTts.uploadPage = uploadPage;
